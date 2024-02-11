2e72e405468b03eb6921d1dffe2ca0c2e7604b57
Quintiliano Paquini Nery <quintiliano1995@gmail.com>
2024-02-10 22:41:32 -0300

    Testre

--- config/android/browserstack/prd/wdio.ebac.conf.js
+++ config/android/browserstack/prd/wdio.ebac.conf.js
@@ -1,12 +1,12 @@
 const { config } = require('../../../wdio.shared.conf')
 
 // Definitions for test execution
-const appUrl = 'bs://d3e839b6c0b52383a174b764a189cb1389fcf74f' 
+const appUrl = 'bs://d3e839b6c0b52383a174b764a189cb1389fcf74f'
 const releaseName = 'Release 1.1.1' // Example: Release 1.1.1 - Hotfix 1
 const buildNumber = '00123456'
 
 // Settings of the device to be tested
-const deviceName = 'Samsung Galaxy S10e' 
+const deviceName = 'Samsung Galaxy S10e'
 const deviceVersion = '9.0'
 
 config.specs = [
@@ -18,8 +18,9 @@ config.services = [
     'browserstack',
     {
       app: process.env.BROWSERSTACK_APP_ID || appUrl,
-      // buildIdentifier: "${BUILD_NUMBER}",
+      //  buildIdentifier: "${BUILD_NUMBER}",
       browserstackLocal: true,
+
       testObservability: true,
       testObservabilityOptions: {
         'projectName': 'Automated test APP Escola Ebac',
@@ -34,8 +35,8 @@ config.capabilities = [{
   build: process.env.BROWSERSTACK_BUILD_NAME,
   projectName: 'Automated test APP Escola Ebac',
   name: 'UIAutomator2',
-  deviceName: deviceName,
-  platformVersion: deviceVersion,
+  deviceName: 'Samsung Galaxy S10e',
+  platformVersionplatformVersion: '9.0',
   platformName: 'android',
   autoGrantPermissions: true,
   interactiveDebugging: true,
--- config/android/local/wdio.ebac.conf.js
+++ config/android/local/wdio.ebac.conf.js
@@ -9,16 +9,20 @@ config.specs = [
 
 config.capabilities = [{
     'appium:platformName': 'Android',
-    'appium:platformVersion': '9.0',
-    'appium:deviceName': 'Samsung Galaxy S10e',
+    'appium:platformVersion': '13.0',
+    'appium:deviceName': 'samsung-galaxy-s20',
     'appium:automationName': 'UIAutomator2',
+    'appium:udid': "RX8N305B2CK",
+    'appium:appPackage': "com.woocommerce.android",
+    "appium:appActivity": "com.woocommerce.android.ui.main.MainActivity",
+    "appium:appWaitActivity": "com.woocommerce.android.ui.login.LoginActivity",
     'appium:app': path.join(process.cwd(), 'apps/android/woocommerce.apk'),
     'appium:autoGrantPermissions': true
 }]
 
 config.platform = 'android'
-config.application = 'EBAC'
-config.environment = 'staging'
+config.application = 'ebac'
+config.environment = 'production'
 
 config.services = ['appium']
 
